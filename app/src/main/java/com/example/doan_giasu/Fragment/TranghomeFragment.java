01f5ebe8078b5345cef802ebafafc595fc210d0e
truc1 <truc15112002@gmail.com>
2024-03-02 23:48:17 +0700

    V4: Loi

--- app/src/main/java/com/example/doan_giasu/Fragment/TranghomeFragment.java
+++ app/src/main/java/com/example/doan_giasu/Fragment/TranghomeFragment.java
@@ -70,7 +70,7 @@ public class TranghomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        mView = inflater.inflate(R.layout.fragment_tranghome, container, false);
         xuly();
         return mView;
     }
