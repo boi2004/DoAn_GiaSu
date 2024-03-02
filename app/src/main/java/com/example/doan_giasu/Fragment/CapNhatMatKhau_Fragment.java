dcb8a11957dcf7a205e2cb5ad948b5cf309a39a9
boi2004 <alorota24@gmail.com>
2024-03-02 15:54:11 +0700

    Update_Nguyen_Fragment_v2

--- app/src/main/java/com/example/doan_giasu/Fragment/CapNhatMatKhau_Fragment.java
+++ app/src/main/java/com/example/doan_giasu/Fragment/CapNhatMatKhau_Fragment.java
@@ -12,10 +12,10 @@ import com.example.doan_giasu.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DangKyLamGiaSu_fra#newInstance} factory method to
+ * Use the {@link CapNhatMatKhau_Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DangKyLamGiaSu_fra extends Fragment {
+public class CapNhatMatKhau_Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class DangKyLamGiaSu_fra extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DangKyLamGiaSu_fra() {
+    public CapNhatMatKhau_Fragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class DangKyLamGiaSu_fra extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DangKyLamGiaSu.
+     * @return A new instance of fragment CapNhatMatKhau_Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DangKyLamGiaSu_fra newInstance(String param1, String param2) {
-        DangKyLamGiaSu_fra fragment = new DangKyLamGiaSu_fra();
+    public static CapNhatMatKhau_Fragment newInstance(String param1, String param2) {
+        CapNhatMatKhau_Fragment fragment = new CapNhatMatKhau_Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class DangKyLamGiaSu_fra extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dang_ky_lam_gia_su, container, false);
+        return inflater.inflate(R.layout.fragment_capnhatmatkhau, container, false);
     }
 }
--- app/src/main/java/com/example/doan_giasu/Fragment/SetFarmMent_Activity.java
+++ app/src/main/java/com/example/doan_giasu/Fragment/SetFarmMent_Activity.java
@@ -9,17 +9,22 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.example.doan_giasu.Fragment.HomeFragment;
-import com.example.doan_giasu.Fragment.InformationFragment;
+import com.example.doan_giasu.Dangnhap_Activity;
 import com.example.doan_giasu.R;
 import com.google.android.material.navigation.NavigationView;
 
 public class SetFarmMent_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final int FRAGMENT_HOME = 0;
     private static final int FRAGMENT_INFOMATION = 1;
+    private static final int FRAGMENT_DANHSACHLOPDAY = 2;
+    private static final int FRAGMENT_DANHSACHLOPHOC = 3;
+    private static final int FRAGMENT_CAPNHATMATKHAU = 4;
+    private static final int FRAGMENT_DANGKYLAMGIASU = 5;
+    private static final int FRAGMENT_DIEUKHOANVADICHVU = 6;
 
     private  int mCurrentFragment = FRAGMENT_HOME;
     private DrawerLayout mDrawerLayout;
@@ -49,22 +54,56 @@ public class SetFarmMent_Activity extends AppCompatActivity implements Navigatio
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.nav_Trangchu){
-            if (mCurrentFragment != FRAGMENT_HOME){
+            if (mCurrentFragment != FRAGMENT_HOME){             //FRAMENT TRANG CHỦ
                 replaceFragment(new HomeFragment());
                 mCurrentFragment = FRAGMENT_HOME;
             }
 
-        }else if (id == R.id.nav_Thongtincanhan){
+        }else if (id == R.id.nav_Dangkylamgiasu){               //FRAMENT ĐĂNG KÝ LÀM GIA SƯ
+            if (mCurrentFragment != FRAGMENT_DANGKYLAMGIASU){
+                replaceFragment(new DangKyLamGiaSu_Fragment());
+                mCurrentFragment = FRAGMENT_DANGKYLAMGIASU;
+            }
+
+        }else if (id == R.id.nav_Danhsachlopday){              //FRAMENT DANH SÁCH LỚP DẠY
+            if (mCurrentFragment != FRAGMENT_DANHSACHLOPDAY){
+                replaceFragment(new DanhSachLopDay_Fragment());
+                mCurrentFragment = FRAGMENT_DANHSACHLOPDAY;
+            }
+
+        }else if (id == R.id.nav_Doimatkhau){               //FRAMENT DANH SÁCH LỚP HỌC
+            if (mCurrentFragment != FRAGMENT_CAPNHATMATKHAU){
+                replaceFragment(new CapNhatMatKhau_Fragment());
+                mCurrentFragment = FRAGMENT_CAPNHATMATKHAU;
+            }
+
+        }else if (id == R.id.nav_Thongtincanhan){           //FRAMNENT THÔNG TIN CÁ NHÂN
             if (mCurrentFragment != FRAGMENT_INFOMATION){
                 replaceFragment(new InformationFragment());
                 mCurrentFragment = FRAGMENT_INFOMATION;
             }
 
+        }else if (id == R.id.nav_dieukhoanvadichvu){        //FRAMENT ĐIỀU KHOẢN VÀ DỊCH VỤ
+            if (mCurrentFragment != FRAGMENT_DIEUKHOANVADICHVU){
+                replaceFragment(new Dieukhoanvadichvu_Fragment());
+                mCurrentFragment = FRAGMENT_DIEUKHOANVADICHVU;
+            }
+        }else if (id == R.id.nav_Danhsachlophoc){           //FRAMENT DANH SÁCH LỚP HỌC
+            if (mCurrentFragment != FRAGMENT_DANHSACHLOPHOC){
+                replaceFragment(new DanhSachLopHoc_Fragment());
+                mCurrentFragment = FRAGMENT_DANHSACHLOPHOC;
+            }
+        }else if (id == R.id.nav_Dangxuat){           //FRAMENT Đăng xuất
+            performLogout();
         }
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-
+    private void performLogout() {
+        Intent intent = new Intent(this, Dangnhap_Activity.class);      //Hàm đăng xuất để gọi trong dangxuat
+        startActivity(intent);
+        finish();
+    }
     private void replaceFragment(Fragment fragment){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.content_frame, fragment);
